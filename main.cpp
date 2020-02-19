f4e77a9ecc78fe27237e01a6d854a2a7d5e63349
Kohlmann András <metuoku@outlook.com>
2020-02-19 07:20:01 +0100

    init

---
+++ main.cpp
@@ -0,0 +1,81 @@
+#include <algorithm>
+#include <iterator>
+#include <vector>
+#include <chrono>
+#include <iostream>
+
+using std::back_inserter;
+using std::fill_n;
+using std::vector;
+using namespace std::chrono;
+
+struct Organism {
+    int legs = 2;
+    int arms = 2;
+};
+
+struct Person : Organism {
+    int age = 34;
+    float height = 180.1;
+    float weight = 78.3;
+    std::string name = "Gipsz Jakab";
+
+    void get_older() { age++; }
+};
+
+static void ArrayOfStructs(int N) {
+    vector<Person> v;
+    fill_n(back_inserter(v), N, Person());
+
+    for (int i = 0; i < N; ++i) {
+        for (auto &i : v) {
+            i.get_older();
+        }
+    }
+}
+
+struct Components {
+    Components(int N) {
+        legs.resize(N, 2);
+        arms.resize(N, 2);
+        ages.resize(N, 34);
+        heights.resize(N, 180.1);
+        weights.resize(N, 78.3);
+        names.resize(N, "Gipsz Jakab");
+    }
+
+    void get_older() {
+        for (auto &i : ages) {
+            i++;
+        }
+    }
+
+    vector<int> legs;
+    vector<int> arms;
+    vector<int> ages;
+    vector<float> heights;
+    vector<float> weights;
+    vector<std::string> names;
+};
+
+static void StructOfArrays(int N) {
+    Components struct_of_arrays(N);
+    for (int i = 0; i < N; ++i) {
+        struct_of_arrays.get_older();
+    }
+}
+
+static int measure_runtime(void (*f)(int), int N) {
+    auto start = high_resolution_clock::now();
+    for (int i = 0; i < 5; i++) f(N);
+    return duration_cast<microseconds>(high_resolution_clock::now() - start).count() / 5;
+}
+
+int main() {
+    int N = 64;
+    for (int i = 0; i < 8; ++i) {
+        N = N << 1;
+        std::cout << measure_runtime(ArrayOfStructs, N) << " vs ";
+        std::cout << measure_runtime(StructOfArrays, N) << std::endl;
+    }
+}
